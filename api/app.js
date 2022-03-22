4ec6c39d863d12c43a12b276f0fb95de975778ea
tomasz kowalczyk <tomasz.kowalczyk1983@gmail.com>
2022-03-22 21:16:32 +0000

    add cors to api

--- api/app.js
+++ api/app.js
@@ -7,11 +7,13 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const prometheus = require('express-prometheus-middleware')
 const authMiddleware = require('./middleware/auth')
+var cors = require('cors')
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
